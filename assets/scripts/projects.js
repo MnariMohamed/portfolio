c904529dde19cc8143d75d1fe67ff4f21ff86288
M'nari Mohamed <medmnari99@gmail.com>
2023-01-28 02:31:58 +0100

    add project

--- assets/scripts/projects.js
+++ assets/scripts/projects.js
@@ -1,33 +1,21 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = [1,1];
+let slideId = ["mySlides1", "mySlides2"]
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+for (let index = 0; index < slideId.length; index++) {
+  showSlides(1, index);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
-function showSlides(n) {
+function showSlides(n, no) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  x[slideIndex[no]-1].style.display = "block";  
 }
-
-// add a list of work to the html
-function addWork(work) {
-    let workList = document.querySelector('.row work-row');
-    workList.innerHTML += work;
-    }
-
