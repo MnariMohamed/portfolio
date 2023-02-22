52e87187381a7683382dd328fe2f33f5275acce1
M'nari Mohamed <medmnari99@gmail.com>
2023-02-22 14:49:01 +0100

    changes

--- assets/scripts/projects.js
+++ assets/scripts/projects.js
@@ -1,4 +1,4 @@
-let slideIndex = [1,1,1];
+let slideIndex = [1, 1, 1, 1];
 let slideId = [];
 
 function initSlides() {
@@ -20,10 +20,10 @@ function plusSlides(n, no) {
 function showSlides(n, no) {
   let i;
   let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
+  if (n > x.length) { slideIndex[no] = 1 }
+  if (n < 1) { slideIndex[no] = x.length }
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
-  x[slideIndex[no]-1].style.display = "block";  
+  x[slideIndex[no] - 1].style.display = "block";
 }
